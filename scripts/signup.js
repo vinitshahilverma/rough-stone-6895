6bcd74d76b42ea79bcb6bd07808e8e6d4e4464a2
dileep8920 <dileepkumary669@gmail.com>
2022-07-21 22:30:25 +0530

    login done

---
+++ scripts/signup.js
@@ -0,0 +1,48 @@
+let form=document.querySelector("form")
+let userData=JSON.parse(localStorage.getItem("userData")) || []
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let data={
+        email:form.email.value,
+        name:form.name.value,
+        date:form.date.value,
+        password:form.password.value,
+        pass2:form.pass2.value,
+    }
+    if(data.email==""||data.name==""||data.password==""||data.pass2==""||data.date==""){
+        alert("Please enter correct details")
+    }
+    else{
+        if(checkname(data.name)==true){
+            userData.push(data)
+            localStorage.setItem("userData", JSON.stringify(userData))
+            window.location.href = "login.html"
+        }else{
+            alert("account already exists")
+        }
+    }
+   
+})
+
+
+function checkname(name){
+    let filtered=userData.filter(function(element){
+        return name ===element.name;
+    })
+    if(filtered.length>0){
+        return false
+    }else{
+        return true
+    }
+}
+
+function body(){
+    document.getElementById("main").innerHTML=""
+    let form = document.createElement("form")
+
+    let label=document.createElement("label")
+    label.innerText="Height"
+    let input=document.createElement("input")
+    input.setAttribute("type","text")
+
+}
