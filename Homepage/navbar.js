153082bd2364e36f38ef9581aa79230a6e0633d9
Radhe Shyam Soni <sradhe545@gmail.com>
2022-07-23 18:37:49 +0530

    added day5 files

---
+++ Homepage/navbar.js
@@ -0,0 +1,15 @@
+function navbar(){
+    return `  
+    <div class="logo"><a href="fb.html"><img src="https://www.gymwolf.com/img/gymwolf.png" alt=""></a> </div>
+    <ul>
+        <li><a href="">Progres</a></li>
+        <li><a href="">Calculators</a></li>
+        <li><a href="">Exercise and plans</a></li>
+        <li><a href="">My personal trainer</a></li>
+        <li><a  id="sigin" href="">Sign in</a></li>
+        <li><span style="color:white" class="material-symbols-outlined">
+            search
+            </span></li>        
+    </ul>`
+}
+export default navbar
