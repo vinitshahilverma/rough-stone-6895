fef3c1bb54cbbca230f937c7ae7b058db855e00e
vinitshahilverma <vinitshahilverma@gmail.com>
2022-07-24 09:00:42 +0530

    change

--- scriptss/main.js
+++ scriptss/main.js
@@ -5,17 +5,26 @@ document.querySelector("#mainDiv").innerHTML = header();
 document.querySelector("#list").innerHTML = list();
 
 
+import footer from "../Homepage/footer.js"
+
+document.querySelector("#footer-top").innerHTML = footer()
+
+import navbar from "../Homepage/navbar.js"
+document.querySelector("#nav").innerHTML = navbar()
+
+// shahil
+
 let  workout = [
 
-    {img:"https://www.gymwolf.com/images/category_chest.jpg",heading:"Chest"},
-    {img:"https://www.gymwolf.com/images/category_biceps.jpg",heading:"Biceps"},
-    {img:"https://www.gymwolf.com/images/category_triceps.jpg",heading:"Triceps"},
-    {img:"https://www.gymwolf.com/images/category_back.jpg",heading:"Back"},
-    {img:"https://www.gymwolf.com/images/category_shoulder.jpg",heading:"Shoulder"},
-    {img:"https://www.gymwolf.com/images/category_legs.jpg",heading:"Leg"},
-    {img:"https://www.gymwolf.com/images/category_abdominal.jpg",heading:"Abdominal"},
-    {img:"https://www.gymwolf.com/images/category_combined.jpg",heading:"Combined"},
-    {img:"https://www.gymwolf.com/images/category_cardio.jpg",heading:"Cardio"},
+    {image:"https://www.gymwolf.com/images/category_chest.jpg",body_name:"Chest"},
+    {image:"https://www.gymwolf.com/images/category_biceps.jpg",body_name:"Biceps"},
+    {image:"https://www.gymwolf.com/images/category_triceps.jpg",body_name:"Triceps"},
+    {image:"https://www.gymwolf.com/images/category_back.jpg",body_name:"Back"},
+    {image:"https://www.gymwolf.com/images/category_shoulder.jpg",body_name:"Shoulder"},
+    {image:"https://www.gymwolf.com/images/category_legs.jpg",body_name:"Leg"},
+    {image:"https://www.gymwolf.com/images/category_abdominal.jpg",body_name:"Abdominal"},
+    {image:"https://www.gymwolf.com/images/category_combined.jpg",body_name:"Combined"},
+    {image:"https://www.gymwolf.com/images/category_cardio.jpg",body_name:"Cardio"},
 
 ]
 
@@ -25,26 +34,26 @@ let  workout = [
 let picture = (workout) => {
     
     workout.forEach((el) => {
-        let {img,heading}=el;
+        let {image,body_name}=el;
 
         let div=document.createElement("div");
         div.addEventListener("click",() =>{
              localStorage.setItem("body_name",JSON.stringify(el));
-             window.location.href = "chest.html";
+             window.location.href = "../sandy_gymwolf/chest.html";
         })
       
-        let image= document.createElement("img");
-        image.src=img;
+        let image1= document.createElement("img");
+        image1.src=image;
         
         let smallDiv=document.createElement("div");
         smallDiv.setAttribute("class","smallDiv");
 
         let h1 = document.createElement("h2");
-         h1.innerText=heading;
+         h1.innerText=body_name;
 
          smallDiv.append(h1);
 
-         div.append(image,smallDiv);
+         div.append(image1,smallDiv);
     
 
          document.querySelector("#picture").append(div)
@@ -57,7 +66,7 @@ let abdominal = document.querySelector("#abdominal");
 
 
 let append = () => {
-let url = "http://127.0.0.1:3000/api/abdominal"
+let url = "http://localhost:3000/Abdominal"
 
 fetch(url)
 .then((res)=>{
@@ -95,6 +104,9 @@ let displayTable = (data,abdominal) => {
     }
 
       let h3 = document.createElement("h3");
+      h3.addEventListener("click",()=>{
+           myFunction(el);
+      })
       h3.innerText = title;
       h3.setAttribute("class","h3")
 
@@ -114,7 +126,7 @@ let displayTable = (data,abdominal) => {
 let back = document.querySelector("#back");
 
 let append1 = () => {
-    let url = "http://127.0.0.1:3000/api/back"
+    let url = "http://localhost:3000/Back"
     
     fetch(url)
     .then((res)=>{
@@ -133,7 +145,7 @@ let append1 = () => {
  let biceps = document.querySelector("#biceps");
 
 let append2 = () => {
-    let url = "http://127.0.0.1:3000/api/biceps"
+    let url = "http://localhost:3000/Biceps"
     
     fetch(url)
     .then((res)=>{
@@ -153,7 +165,7 @@ let append2 = () => {
  let chest = document.querySelector("#chest");
 
 let append3 = () => {
-    let url = "http://127.0.0.1:3000/api/chest"
+    let url = "http://localhost:3000/Chest"
     
     fetch(url)
     .then((res)=>{
@@ -173,7 +185,7 @@ let append3 = () => {
  let combined = document.querySelector("#combined");
 
 let append4 = () => {
-    let url = "http://127.0.0.1:3000/api/combined"
+    let url = "http://localhost:3000/Combined"
     
     fetch(url)
     .then((res)=>{
@@ -192,7 +204,7 @@ let append4 = () => {
  let legs = document.querySelector("#legs");
 
 let append5 = () => {
-    let url = "http://127.0.0.1:3000/api/legs"
+    let url = "http://localhost:3000/Legs"
     
     fetch(url)
     .then((res)=>{
@@ -211,7 +223,7 @@ let append5 = () => {
  let shoulder = document.querySelector("#shoulder");
 
 let append7 = () => {
-    let url = "http://127.0.0.1:3000/api/shoulder"
+    let url = "http://localhost:3000/Shoulder"
     
     fetch(url)
     .then((res)=>{
@@ -230,7 +242,7 @@ let append7 = () => {
  let triceps = document.querySelector("#triceps");
 
 let append8 = () => {
-    let url = "http://127.0.0.1:3000/api/triceps"
+    let url = "http://localhost:3000/Triceps"
     
     fetch(url)
     .then((res)=>{
@@ -245,4 +257,33 @@ let append8 = () => {
     
  append8();
 
+ let myFunction = (el) => {
+    if(el.video==""){
+        localStorage.setItem("data",JSON.stringify(el));
+        window.location.href = "../sandy_gymwolf/chestobj.html"
+    }
+    else{
+    localStorage.setItem("data",JSON.stringify(el))
+    window.location.href = "../sandy_gymwolf/video.html"
+    }
+ }
 
+
+
+ let one = () => {
+    window.location.href = "../frontend/main.html"
+}
+
+
+let two = () => {
+    window.location.href = "../frontend/myexercise.html"
+}
+
+
+let three = () => {
+    window.location.href = "../frontend/workout.html"
+}
+
+document.querySelector("#one").addEventListener("click",one);
+document.querySelector("#two").addEventListener("click",two);
+document.querySelector("#three").addEventListener("click",three);
