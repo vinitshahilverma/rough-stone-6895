43a47f0695e54cd9c52140f3fc7fe5962b710dd9
Radhe Shyam Soni <sradhe545@gmail.com>
2022-07-24 09:37:05 +0530

    added

--- Homepage/navbar.js
+++ Homepage/navbar.js
@@ -4,8 +4,8 @@ function navbar(){
         <p><a id="ab1" href="../sandy_gymwolf/progress.html">Progress</a></p>
         <p><a id="ab2" href="">Calculators</a></p>
         <p><a  id="ab3" href="../frontend/main.html">Exercise and plans</a></p>
-        <p><a id="ab4" href="./my_trainer.html">My personal trainer</a></p>
-        <p><a id="ab5" id="sigin" href="">Sign in</a></p>
+        <p><a id="ab4" href="../my_trainer.html">My personal trainer</a></p>
+        <p><a id="ab5" id="sigin" href="./login.html">Sign in</a></p>
         <span style="color:white" class="material-symbols-outlined">search</span>
     </div> ` 
    
